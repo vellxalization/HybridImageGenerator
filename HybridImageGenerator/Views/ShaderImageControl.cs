fe0ebba1bca9c42ccce8d2127ac81327543492f5
vellxalization <abcd1234abcd12346738t68374@gmail.com>
2026-02-10 20:11:05 +0300

    direct properties

--- HybridImageGenerator/Views/ShaderImageControl.cs
+++ HybridImageGenerator/Views/ShaderImageControl.cs
@@ -8,34 +8,37 @@ using SkiaSharp;
 namespace HybridImageGenerator.Views;
 
 public class ShaderImageControl : Control {
-    public static readonly StyledProperty<Size> ImageSizeProperty =
-        AvaloniaProperty.Register<ShaderImageControl, Size>(nameof(ImageSize));
-
+    public static readonly DirectProperty<ShaderImageControl, Size> ImageSizeProperty =
+        AvaloniaProperty.RegisterDirect<ShaderImageControl, Size>(nameof(ImageSize),
+            owner => owner.ImageSize,
+            (owner, size) => owner.ImageSize = size);
+    
     public Size ImageSize {
-        get => GetValue(ImageSizeProperty);
-        set => SetValue(ImageSizeProperty, value);
+        get;
+        set => SetAndRaise(ImageSizeProperty, ref field, value);
     }
     
-    public static readonly StyledProperty<SKShader?> InputShaderProperty =
-        AvaloniaProperty.Register<ShaderImageControl, SKShader?>(nameof(InputShader));
+    public static readonly DirectProperty<ShaderImageControl, SKShader?> InputShaderProperty =
+        AvaloniaProperty.RegisterDirect<ShaderImageControl, SKShader?>(nameof(InputShader),
+            owner => owner.InputShader,
+            (owner, shader) => owner.InputShader = shader);
 
     public SKShader? InputShader {
-        get => GetValue(InputShaderProperty);
-        set => SetValue(InputShaderProperty, value);
+        get;
+        set => SetAndRaise(InputShaderProperty, ref field, value);
     }
-    
+
     private readonly SKPaint _paint = new SKPaint();
 
     static ShaderImageControl() {
         InputShaderProperty.Changed.AddClassHandler<ShaderImageControl>(
-            (x, e) => x._paint.Shader = e.GetNewValue<SKShader?>());
-        AffectsRender<ShaderImageControl>(InputShaderProperty);
-        AffectsRender<ShaderImageControl>(ImageSizeProperty);
+            (owner, args) => owner._paint.Shader = args.GetNewValue<SKShader?>());
+        AffectsRender<ShaderImageControl>(InputShaderProperty, ImageSizeProperty);
     }
     
     public override void Render(DrawingContext context) {
         if (InputShader is null || InputShader.Handle == IntPtr.Zero) return;
-
+        
         var renderRect = CalculateRenderRect(Bounds, ImageSize);
         var drawOperation = new DrawToUIOperation(_paint, renderRect.ToAvaloniaRect());
         context.Custom(drawOperation);
