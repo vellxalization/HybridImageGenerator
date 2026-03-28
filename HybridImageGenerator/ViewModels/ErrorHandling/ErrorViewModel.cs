e5cf00e6f672c485669cfef2ae253aaecef4ef67
vellxalization <abcd1234abcd12346738t68374@gmail.com>
2026-03-29 01:12:32 +0300

    better visuals for dialogs

--- HybridImageGenerator/ViewModels/ErrorHandling/ErrorViewModel.cs
+++ HybridImageGenerator/ViewModels/ErrorHandling/ErrorViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DialogHostAvalonia;
 using HybridImageGenerator.Models;
@@ -14,23 +17,33 @@ public abstract partial class ErrorViewModel : ViewModelBase {
     [ObservableProperty]
     private string _criticalButtonText;
     [ObservableProperty]
-    private bool _stackTraceVisible;
-    [ObservableProperty]
     private bool _isVisible;
 
     [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(ContinueCommand))]
     private bool _isFatalError;
+
+    [ObservableProperty]
+    private double _stackTraceOpacity;
     
     protected ErrorViewModel(string criticalButtonText, ErrorDetails details) {
         _criticalButtonText = criticalButtonText;
         _isFatalError = details.IsFatal;
         _message = details.Message;
-        _stackTrace = details.StackTrace;
+        _stackTrace = details.StackTrace is null ? null : PrepareStackTrace(details.StackTrace);
+    }
+
+    private static string PrepareStackTrace(string stackTrace) {
+        string[] split = stackTrace.Split(Environment.NewLine);
+        IEnumerable<string> trimmed = split.Select(line => line.TrimStart(' '));
+        return string.Concat(string.Join(Environment.NewLine, trimmed), Environment.NewLine); 
+        // append newline for better scroll bar visibility
     }
     
-    [RelayCommand(CanExecute=nameof(CanShowStackTrace))]
-    private void ShowStackTraceSwitch() => StackTraceVisible = !StackTraceVisible;
+    [RelayCommand(CanExecute = nameof(CanShowStackTrace))]
+    private void ShowStackTraceSwitch() {
+        StackTraceOpacity = StackTraceOpacity > 0 ? 0 : 1;
+    }
 
     private bool CanShowStackTrace() => StackTrace is not null;
 
